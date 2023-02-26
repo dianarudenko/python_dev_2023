0fcc9647fbd880f0838fe8faef2b1499ed7d2ccd
Diana Rudenko <dianarudenko@yandex.ru>
2023-02-26 18:54:18 +0300

    Main code done

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,9 +1,12 @@
 from random import choice
+from argparse import ArgumentParser
+import os
+from urllib import request
 
 def bullscows(guess: str, secret: str) -> (int, int):
     bulls, cows = 0, 0
-    for i in range(min(len(guess), len(secret))):
-        if guess[i] == secret[i]:
+    for i in range(len(guess)):
+        if i < len(secret) and guess[i] == secret[i]:
             bulls += 1
         elif guess[i] in secret:
             cows += 1
@@ -14,7 +17,11 @@ def ask(prompt: str, valid: list[str] = None) -> str:
     word = input(prompt)
     if valid:
         while not word in valid:
-            print('Невалидное слово, попробуйте снова')
+            print('Плохое слово, попробуйте снова')
+            word = input(prompt)
+    else:
+        while len(word) != args.length:
+            print(f'Слово должно быть длины {args.length}, попробуйте снова')
             word = input(prompt)
     return word
 
@@ -33,4 +40,27 @@ def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
         inform("Быки: {}, Коровы: {}", bulls, cows)
         count += 1
     return count
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('dict', type=str, help='Name or URL of file with the dictionary')
+    parser.add_argument('length', type=int, nargs='?', default=5)
+    args = parser.parse_args()
+
+    dictionary = []
+    if os.path.exists(args.dict):
+        with open(args.dict) as inp:
+            dictionary = inp.read().split()
+    else:
+        try:
+            with request.urlopen(args.dict) as inp:
+                dictionary = inp.read().decode('utf-8').split()
+        except:
+            print('Невалидная URL')
+    dictionary = [word for word in dictionary if len(word) == args.length]
+    if dictionary == []:
+        print('Пустой словарь доступных слов, игра невозможна')
+    else:
+        attempts = gameplay(ask, inform, dictionary)
+        print(f'Победа! Потребовалось попыток: {attempts}')
     
